22615040d60177baff61b6967cd62e1e50ebde44
muratuenlue <muratuenlue@gmail.com>
2018-08-09 14:15:51 +0400

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ exports.run = (client, message, params) => {
   .setDescription('')
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `	O!banned = Dene ve Gör! \nO!avatarım = Avatarınınızı Gösterir. \nO!herkesebendençay = Herkese Çay Alırsınız. \nO!koş = Koşarsınız.\nO!çayiç = Çay İçersiniz. \nO!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nO!çayaşekerat = Çaya Şeker Atarsınız. \nO!yumruh-at = Yumruk Atarsınız. \nO!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nO!sunucuresmi = BOT Sunucunun Resmini Atar. \nO!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nO!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
-  .addField("**Yetkilisi Komutlar**", `!Otemizle=Belirlenen miktar mesajı siler. \nO!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nO!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nO!unban = İstediğiniz Kişinin Yasağını Açar. \nO!sustur = İstediğiniz Kişiyi Susturur. \nO!oylama = Oylama Açar. \nO!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
+  .addField("**Yetkilisi Komutlar**", `O!temizle=Belirlenen miktar mesajı siler. \nO!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nO!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nO!unban = İstediğiniz Kişinin Yasağını Açar. \nO!sustur = İstediğiniz Kişiyi Susturur. \nO!oylama = Oylama Açar. \nO!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
   .addField("**Ana Komutlar**", "O!yardım = BOT Komutlarını Atar. \nO!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nO!ping = BOT Gecikme Süresini Söyler. \nO!davet = BOT Davet Linkini Atar. \nO!istatistik = BOT İstatistiklerini Atar.")
   .addField("**Yapımcı**", " **OR |💻OyuncuReisHD#0421** ")
   .setFooter('**--------------------------**')
