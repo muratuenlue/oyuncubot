db3eaf1dd48642794842e3901c7fd76b982999b6
muratuenlue <muratuenlue@gmail.com>
2018-11-12 22:37:37 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("O!yardım | " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ","");
+client.user.setGame("_yardım | " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ","");
 }
